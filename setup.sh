01f45a2af77741aa5905003d1bb6bd1b1599c52c
Hardik Shah <hardikns@gmail.com>
2015-02-19 10:43:39 +0530

    Adding setup.sh file to setting up Salt

---
+++ setup.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+addrepos() {
+    sudo add-apt-repository ppa:saltstack/salt  
+    sudo apt-get update  
+}
+
+saltminion() {
+    sudo apt-get install salt-minion
+    read -p "Ready to update the salt-minion file [Y/n]:" choice
+    if [ $choice != "n" ] ; then
+      sudo nano /etc/salt/minion  
+      read -p "Ready to update the salt-minion id [Y/n]:" choice2
+      if [ $choice2 != "n" ] ; then
+        sudo nano /etc/salt/minion  
+      fi
+      read -p "Ready to restart server [Y/n]:" choice2
+      if [ $choice2 != "n" ] ; then
+        sudo service salt-minion restart
+      fi
+    fi
+}
+
+saltmaster() {
+    sudo apt-get install salt-master
+    read -p "Ready to update the salt-master file [Y/n]:" choice
+    if [ $choice != "n" ] ; then
+      sudo nano /etc/salt/master  
+      read -p "Ready to restart server [Y/n]:" choice2
+      if [ $choice2 != "n" ] ; then
+        sudo service salt-master restart
+      fi
+    fi
+}
+
+read -p "Configuring this as Salt-Master [Y/n]:" choice
+#addrepos
+if [ $choice == 'n' ]; then
+    saltminion
+else
+    saltmaster
+fi
+
+
